9e1f46a6322a0fce3f4de5f44b5e6085853042cf
nitan <vhb@vhb.dk>
2023-07-20 18:18:05 +0200

    Increase player items per lot

--- MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
@@ -923,7 +923,7 @@ void BuildingObjectImplementation::onExit(CreatureObject* player, uint64 parenti
 
 uint32 BuildingObjectImplementation::getMaximumNumberOfPlayerItems() {
 	if (isCivicStructure() )
-		return 250;
+		return 1000;
 
 	SharedStructureObjectTemplate* ssot = dynamic_cast<SharedStructureObjectTemplate*> (templateObject.get());
 
@@ -939,7 +939,7 @@ uint32 BuildingObjectImplementation::getMaximumNumberOfPlayerItems() {
 
 	auto maxItems = MAXPLAYERITEMS;
 
-	return Math::min(maxItems, lots * 100);
+	return Math::min(maxItems, lots * 300);
 }
 
 int BuildingObjectImplementation::notifyObjectInsertedToChild(SceneObject* object, SceneObject* child, SceneObject* oldParent) {
