f8a0a1099918a3bf21062296c8cbc6b9358df190
nitan <vhb@vhb.dk>
2023-07-20 18:03:47 +0200

    Jedi Unlock Hologrind

--- MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
@@ -3,8 +3,8 @@ local ObjectManager = require("managers.object.object_manager")
 
 jediManagerName = "HologrindJediManager"
 
-NUMBEROFPROFESSIONSTOMASTER = 6
-MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = NUMBEROFPROFESSIONSTOMASTER - 2
+NUMBEROFPROFESSIONSTOMASTER = 5
+MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = 0
 
 HologrindJediManager = JediManager:new {
 	screenplayName = jediManagerName,
@@ -18,49 +18,38 @@ HologrindJediManager = JediManager:new {
 function HologrindJediManager:getGrindableProfessionList()
 	local grindableProfessions = {
 		-- String Id, badge number, profession name
-		--{ "pilot_rebel_navy_corellia", 	PILOT_REBEL_NAVY_CORELLIA },
-		--{ "pilot_imperial_navy_corellia", 	PILOT_IMPERIAL_NAVY_CORELLIA },
-		--{ "pilot_neutral_corellia", 		PILOT_CORELLIA },
-		--{ "pilot_rebel_navy_tatooine", 	PILOT_REBEL_NAVY_TATOOINE },
-		--{ "pilot_imperial_navy_naboo", 	PILOT_IMPERIAL_NAVY_NABOO },
-		{ "crafting_architect_master", 		CRAFTING_ARCHITECT_MASTER  },
-		{ "crafting_armorsmith_master", 	CRAFTING_ARMORSMITH_MASTER  },
-		{ "crafting_artisan_master", 		CRAFTING_ARTISAN_MASTER  },
-		{ "outdoors_bio_engineer_master", 	OUTDOORS_BIOENGINEER_MASTER  },
-		{ "combat_bountyhunter_master", 	COMBAT_BOUNTYHUNTER_MASTER  },
-		{ "combat_brawler_master", 		COMBAT_BRAWLER_MASTER  },
-		{ "combat_carbine_master", 		COMBAT_CARBINE_MASTER  },
-		{ "crafting_chef_master", 		CRAFTING_CHEF_MASTER  },
-		{ "science_combatmedic_master", 	SCIENCE_COMBATMEDIC_MASTER  },
-		{ "combat_commando_master", 		COMBAT_COMMANDO_MASTER  },
-		{ "outdoors_creaturehandler_master", 	OUTDOORS_CREATUREHANDLER_MASTER  },
-		{ "social_dancer_master", 		SOCIAL_DANCER_MASTER  },
-		{ "science_doctor_master", 		SCIENCE_DOCTOR_MASTER  },
-		{ "crafting_droidengineer_master", 	CRAFTING_DROIDENGINEER_MASTER  },
-		{ "social_entertainer_master", 		SOCIAL_ENTERTAINER_MASTER  },
-		{ "combat_1hsword_master", 		COMBAT_1HSWORD_MASTER  },
-		{ "social_imagedesigner_master", 	SOCIAL_IMAGEDESIGNER_MASTER  },
-		{ "combat_marksman_master", 		COMBAT_MARKSMAN_MASTER  },
-		{ "science_medic_master", 		SCIENCE_MEDIC_MASTER  },
-		{ "crafting_merchant_master", 		CRAFTING_MERCHANT_MASTER  },
-		{ "social_musician_master", 		SOCIAL_MUSICIAN_MASTER  },
-		{ "combat_polearm_master", 		COMBAT_POLEARM_MASTER  },
-		{ "combat_pistol_master", 		COMBAT_PISTOL_MASTER  },
-		--{ "social_politician_master", 	SOCIAL_POLITICIAN_MASTER  },
-		{ "outdoors_ranger_master", 		OUTDOORS_RANGER_MASTER  },
-		{ "combat_rifleman_master", 		COMBAT_RIFLEMAN_MASTER  },
-		{ "outdoors_scout_master", 		OUTDOORS_SCOUT_MASTER  },
-		--{ "crafting_shipwright", 		CRAFTING_SHIPWRIGHT },
-		{ "combat_smuggler_master", 		COMBAT_SMUGGLER_MASTER  },
-		{ "outdoors_squadleader_master", 	OUTDOORS_SQUADLEADER_MASTER  },
-		{ "combat_2hsword_master", 		COMBAT_2HSWORD_MASTER  },
-		{ "crafting_tailor_master", 		CRAFTING_TAILOR_MASTER  },
-		{ "crafting_weaponsmith_master", 	CRAFTING_WEAPONSMITH_MASTER  },
-		--{ "pilot_neutral_naboo", 		PILOT_NEUTRAL_NABOO },
-		--{ "pilot_neutral_tatooine", 		PILOT_TATOOINE },
-		--{ "pilot_imperial_navy_tatooine", 	PILOT_IMPERIAL_NAVY_TATOOINE },
-		{ "combat_unarmed_master", 		COMBAT_UNARMED_MASTER  },
-	--{ "pilot_rebel_navy_naboo", 		PILOT_REBEL_NAVY_NABOO }
+		{ "crafting_architect_master", 		CRAFTING_ARCHITECT_MASTER  }, 		--1
+		{ "crafting_armorsmith_master", 	CRAFTING_ARMORSMITH_MASTER  },  	--2
+		{ "crafting_artisan_master", 		CRAFTING_ARTISAN_MASTER  },		--3
+		{ "outdoors_bio_engineer_master", 	OUTDOORS_BIOENGINEER_MASTER  },		--4
+		{ "combat_bountyhunter_master", 	COMBAT_BOUNTYHUNTER_MASTER  },		--5
+		{ "combat_brawler_master", 		COMBAT_BRAWLER_MASTER  },		--6
+		{ "combat_carbine_master", 		COMBAT_CARBINE_MASTER  },		--7
+		{ "crafting_chef_master", 		CRAFTING_CHEF_MASTER  },		--8
+		{ "science_combatmedic_master", 	SCIENCE_COMBATMEDIC_MASTER  },		--9
+		{ "combat_commando_master", 		COMBAT_COMMANDO_MASTER  },		--10
+		{ "outdoors_creaturehandler_master", 	OUTDOORS_CREATUREHANDLER_MASTER  },	--11
+		{ "social_dancer_master", 		SOCIAL_DANCER_MASTER  },		--12
+		{ "science_doctor_master", 		SCIENCE_DOCTOR_MASTER  },		--13
+		{ "crafting_droidengineer_master", 	CRAFTING_DROIDENGINEER_MASTER  },	--14
+		{ "social_entertainer_master", 		SOCIAL_ENTERTAINER_MASTER  },		--15
+		{ "combat_1hsword_master", 		COMBAT_1HSWORD_MASTER  },		--16
+		{ "social_imagedesigner_master", 	SOCIAL_IMAGEDESIGNER_MASTER  },		--17
+		{ "combat_marksman_master", 		COMBAT_MARKSMAN_MASTER  },		--18
+		{ "science_medic_master", 		SCIENCE_MEDIC_MASTER  },		--19
+		{ "crafting_merchant_master", 		CRAFTING_MERCHANT_MASTER  },		--20
+		{ "social_musician_master", 		SOCIAL_MUSICIAN_MASTER  },		--21
+		{ "combat_polearm_master", 		COMBAT_POLEARM_MASTER  },		--22
+		{ "combat_pistol_master", 		COMBAT_PISTOL_MASTER  },		--23
+		{ "outdoors_ranger_master", 		OUTDOORS_RANGER_MASTER  },		--24
+		{ "combat_rifleman_master", 		COMBAT_RIFLEMAN_MASTER  },		--25
+		{ "outdoors_scout_master", 		OUTDOORS_SCOUT_MASTER  },		--26
+		{ "combat_smuggler_master", 		COMBAT_SMUGGLER_MASTER  },		--27
+		{ "outdoors_squadleader_master", 	OUTDOORS_SQUADLEADER_MASTER  },		--28
+		{ "combat_2hsword_master", 		COMBAT_2HSWORD_MASTER  },		--29
+		{ "crafting_tailor_master", 		CRAFTING_TAILOR_MASTER  },		--30
+		{ "crafting_weaponsmith_master", 	CRAFTING_WEAPONSMITH_MASTER  },		--31
+		{ "combat_unarmed_master", 		COMBAT_UNARMED_MASTER  },		--32
 	}
 	return grindableProfessions
 end
