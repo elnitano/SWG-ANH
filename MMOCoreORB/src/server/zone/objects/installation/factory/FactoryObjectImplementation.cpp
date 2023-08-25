5e47c949efd2b425825d634feee4dd51e07b66da
nitan <vhb@vhb.dk>
2023-08-25 21:29:46 +0200

    Reduce Factory Time by 50%

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -566,7 +566,7 @@ bool FactoryObjectImplementation::startFactory() {
 	timer = 30;
 	info(true) << "Factory Testing Timer Set To: " << timer;
 #else
-	timer = ((int)schematic->getComplexity()) * 8;
+	timer = ((int)schematic->getComplexity()) * 4;
 #endif
 
 	if (!populateSchematicBlueprint(schematic))
