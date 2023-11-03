6d65afc1c035278fba7ba9df30037b18e9682b1a
nitan <vhb@vhb.dk>
2023-11-03 07:48:17 +0100

    Additional increase to generic loot chance by 10%

--- MMOCoreORB/src/server/zone/managers/loot/lootgroup/LootGroupCollectionEntry.h
+++ MMOCoreORB/src/server/zone/managers/loot/lootgroup/LootGroupCollectionEntry.h
@@ -39,7 +39,7 @@ public:
 		lootChance = lua->getIntField("lootChance");
 
 		if (lootChance == 0) {
-			lootChance = 3000000 + (level * 20000); // 30% + (0.2% * level)
+			lootChance = 4000000 + (level * 20000); // 40% + (0.2% * level)
 		}
 
 		LuaObject luagroups = lua->getObjectField("groups");
