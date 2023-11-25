f74ffd4e6555b3a873bb5d6ee3fed31067d09c66
nitan <vhb@vhb.dk>
2023-11-25 16:32:41 +0100

    Increased damage of crafted Long Vibro Axe

--- MMOCoreORB/bin/scripts/object/weapon/melee/polearm/polearm_vibro_axe.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/polearm/polearm_vibro_axe.lua
@@ -126,8 +126,8 @@ object_weapon_melee_polearm_polearm_vibro_axe = object_weapon_melee_polearm_shar
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "expEffeciency", "expEffeciency", "expEffeciency"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 49, 196, 6.5, 23, 750, -45, -40, 3, -45, 7, 78, 65, 29},
-	experimentalMax = {0, 0, 91, 364, 4.5, 43, 1500, -20, -20, 3, -20, 7, 45, 35, 15},
+	experimentalMin = {0, 0, 74, 294, 6.5, 23, 750, -45, -40, 3, -45, 7, 78, 65, 29}, -- Min changed from 49 to 74 and Max from 196 to 294 - 50% increase
+	experimentalMax = {0, 0, 137, 546, 4.5, 43, 1500, -20, -20, 3, -20, 7, 45, 35, 15}, -- Min changed from 91 to 137 and Max from 364 to 546 - 50% increase
 	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
