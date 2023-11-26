9d5822ea1c356126040d052f30dbeb027d2a284f
nitan <vhb@vhb.dk>
2023-11-26 12:46:29 +0100

    Added /find lots command to list player buildings

--- MMOCoreORB/src/server/zone/objects/creature/commands/FindCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/FindCommand.h
@@ -6,6 +6,7 @@
 #define FINDCOMMAND_H_
 
 #include "server/zone/objects/player/sessions/FindSession.h"
+#include "server/zone/objects/player/sui/messagebox/SuiMessageBox.h"
 
 class FindCommand: public QueueCommand {
 
@@ -50,8 +51,10 @@ public:
 					po->removeWaypointBySpecialType(WaypointObject::SPECIALTYPE_FIND, true);
 
 					return SUCCESS;
-				}
+				}else if (location == "lots") {
 
+					return getLots(creature);
+				}
 				String mapLocType = location;
 
 				if (!mapLocType.isEmpty())
@@ -66,6 +69,70 @@ public:
 			creature->sendSystemMessage("@base_player:find_general_error"); // /Find was unable to complete your request. Please try again.
 
 		}
+
+		return SUCCESS;
+	}
+
+	int getLots(CreatureObject* creature) const {
+		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+
+		if (ghost == nullptr)
+			return GENERALERROR;
+
+		int lotsRemaining = ghost->getLotsRemaining();
+
+		StringBuffer body;
+
+		body << "Player Name:\t" << creature->getFirstName() << endl;
+		body << "Unused Lots:\t" << String::valueOf(lotsRemaining) << endl << endl;
+		body << "Player Structures:";
+
+		for (int i = 0; i < ghost->getTotalOwnedStructureCount(); i++) {
+			ManagedReference<StructureObject*> structure = creature->getZoneServer()->getObject(ghost->getOwnedStructure(i)).castTo<StructureObject*>();
+
+			int num = i + 1;
+			body << endl << String::valueOf(num) << ". ";
+
+			if (structure == nullptr) {
+				body << "nullptr Structure" << endl << endl;
+				continue;
+			}
+
+			String buildingName = structure->getDisplayedName();
+
+			while (buildingName.contains("\\#")) {
+				int nameIndex = buildingName.indexOf("\\#");
+				String sub = "\\" + buildingName.subString(nameIndex, nameIndex + 2);
+				buildingName = buildingName.replaceFirst(sub,"");
+			}
+
+			body << "    Name:\t" << buildingName << endl;
+			body << "    Type:\t" << structure->getObjectTemplate()->getFullTemplateString() << endl;
+			body << "    Lots:\t" << String::valueOf(structure->getLotSize()) << endl;
+			body << "    Remaining Maintenance:\t" << String::valueOf(structure->getSurplusMaintenance()) << endl;
+
+			if (structure->getBasePowerRate() > 0) {
+				body << "    Remaining Power:\t" << String::valueOf(structure->getSurplusPower()) << endl;
+			}
+
+			body << "    Zone:\t";
+			Zone* zone = structure->getZone();
+			if (zone == nullptr) {
+				body << "nullptr" << endl;
+			} else {
+				body << zone->getZoneName() << endl;
+				body << "    World Position:\t" << structure->getWorldPositionX() << ", " << structure->getWorldPositionY() << endl;
+			}
+		}
+
+		ManagedReference<SuiMessageBox*> box = new SuiMessageBox(creature, 0);
+		box->setPromptTitle("Lots for " + creature->getFirstName());
+		box->setPromptText(body.toString());
+		box->setUsingObject(ghost);
+		box->setForceCloseDisabled();
+
+		ghost->addSuiBox(box);
+		creature->sendMessage(box->generateMessage());
 
 		return SUCCESS;
 	}
